e795299fe8635318580a9d96133ec4dc4a391c3f
Saurabh Modh <saurabh.modh@hashedin.com>
2017-05-23 23:09:46 +0530

    imageMin gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
 
 /*
     --- TOP LEVEL FUNCTIONS ---
@@ -22,3 +23,10 @@ gulp.task("default", function(){
 gulp.task("copyHtml", function(){
     gulp.src('src/*.html').pipe(gulp.dest('dist/html'));
 })
+
+// Optimize image
+gulp.task('imageMin', () =>
+    gulp.src('src/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'))
+);
