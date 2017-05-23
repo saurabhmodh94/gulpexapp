d70584ada4d35edeb4168662890ef522053591d2
Saurabh Modh <saurabh.modh@hashedin.com>
2017-05-23 22:39:13 +0530

    Init gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+const gulp = require('gulp');
+
+/*
+    --- TOP LEVEL FUNCTIONS ---
+    gulp.task - Define tasks
+    gulp.src - Point to files to use
+    gulp.dest - Points to output folder
+    gulp.watch - watch file and folders for change
+*/
